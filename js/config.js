f1b01e203168ebc2d8a5bc7fe5922059595a8a92
sarathallaka7993 <sarath.allaka@gmail.com>
2025-11-21 19:10:17 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_4MsEoQb02', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7sqgi2f903idn2pden4tmtt7l6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-north-1' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
